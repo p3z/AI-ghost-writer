4c1416953200d18f2d71e547bcca34aab8363ab4
Linux PC (Shay) <shay@pottleweb.solutions>
2023-01-16 19:38:04 +0000

    Initial commit

---
+++ ghost_writer.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+app.post('/book/:qty/:content_type', async (req, res) => {
+
+  let { qty = 1, content_type } = req.params;
+
+  let acceptable_params = [
+    'title', 'page', 'prologue', 'epilogue', 'blurb', 'tagline', 'summary', 'review',
+  ]
+
+  if(!acceptable_params.includes(content_type)){
+    //console.log("Not acceptable");
+    // Prevent arbitrary inputs
+    res.send({ error: "Not acceptable input" });
+  }
+  
+
+  // let { 
+  //   sentiment,
+  //   length,
+  //   genre,
+  //   summary
+  // } = req.query;
+
+  let user_args = JSON.stringify(req.query);
+  //console.log(user_args)
+
+  let handle_plural_types = (qty == 1) ? content_type : content_type + "s"; // Dont worry about spelling errors, gpt is pretty much smart enough to know what you meant
+
+  let prompt_body = `Using the information expressed in the following json object, generate ${qty} creative ${handle_plural_types} for a book. They should all be completely different\n ${user_args}`;
+
+  // console.log("Generated prompt body")
+  // console.log(prompt_body)
+  
+    
+  let args = {
+      model: "text-davinci-003",  
+      //prompt: "Say hello", // This is just a test line in case something goes wrong with the line below
+      prompt: prompt_body,
+      temperature: 1,
+      max_tokens: 2086,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+  };
+
+  
+  const response = await openai.createCompletion(args);
+  const answer = response.data.choices[0].text;
+
+  console.log("All available options:")
+ // console.log(response.data.choices)
+
+  res.send({ answer });
+});
+
+// Listen for requests
+app.listen(3001, function() {
+	console.log('Server is listening on port 3001');
+});
